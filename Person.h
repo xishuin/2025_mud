423a75cc2035f9e2b5555a20ac7e3ff3154b046a
Zihan Wu <wuzihandacongming@qq.com>
2025-08-25 14:03:18 +0800

    Update Person.h

--- Person.h
+++ Person.h
@@ -5,16 +5,17 @@ using namespace std;
 #define PERSON_H
 class Person{
     protected:
-        int HP, Attack, Defend, Index, x, y;
+        int HP, MaxHP, Attack, Defend, Index, x, y;
         /*
         HP: 生命值
         Index: 位置的索引
         */
         string Name;
     public:
-        Person(int InHP, int InAttack, int InDefend, string InName, int InIndex, int Inx, int Iny):HP(InHP),Attack(InAttack), Defend(InDefend), Name(InName), index(InIndex), x(Inx), y(Iny){}
+        Person(int InHP, int InMaxHP, int InAttack, int InDefend, string InName, int InIndex, int Inx, int Iny):HP(InHP), MaxHP(IntMaxHP),Attack(InAttack), Defend(InDefend), Name(InName), index(InIndex), x(Inx), y(Iny){}
         //获取成员变量
         int GetHP(){ return HP; }
+        int GetMaxHP(){ return MaxHP; }
         string GetName(){ return Name; }
         int GetAttack(){ return Attack; }
         int GetDefend(){ return Defend; }
@@ -38,24 +39,30 @@ class Person{
             return *this;
         }
 };
+
 class Player: public Person{
     private:
         Package PlayerPackage;
         Weapons PlayerWeapons;
         Armor PlayerArmor;
-        int MP, EXP, Level, SkillPoints, Money;
+        int MP, MaxMP, EXP, MaxEXP, Level, Money;
         /*
         MP: 蓝条
         EXP: 经验值
-        SkillPoints: 技能点
         */
         Skill PlayerSkill;
     public:
         //定义Player();
         //修改成员变量
-        bool ChangeMP(int InMP);
-        bool ChangeExp(int InExp); // 涉及到超过限度后自动升级
-        bool ChangeSkillPoints(int InSkillPoints); // 涉及到超过限度后自动升级
+        bool ChangeMP(int InMP); // 最大蓝条
+        bool ChangeEXP(int InExp); // 涉及到超过限度后自动升级
         bool ChangeMoney(int InMoney);
+        //获取成员变量
+        int GetMP(){return MP;}
+        int GetMaxMP(){return MaxMP;}
+        int GetEXP(){return EXP;}
+        int GetMaxEXP(){return MaxEXP;}
+        int GetLevel(){return Level;}
+        int GetMoney(){return Money;}
 };
 #endif //PERSON_H
